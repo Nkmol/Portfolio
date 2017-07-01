bcce646771cc544ffbfe1714a4d4ef889a7f87e1
Nkmol <sandermol95@hotmail.com>
2017-07-01 22:17:09 +0200

    Add awesome navigation hover effect

--- app/src/app/navbar/navbar.component.ts
+++ app/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, QueryList, ElementRef, ViewChildren, ViewChild } from '@angular/core';
+import { Component, OnInit, QueryList, ElementRef, ViewChildren, ViewChild, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'cmp-pf-app-navbar',
@@ -6,12 +6,19 @@ import { Component, OnInit, QueryList, ElementRef, ViewChildren, ViewChild } fro
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  @ViewChild ('containerTrigger') elTriggers : QueryList<ElementRef>;
+  @ViewChild ('action') elAction : ElementRef;
 
-  constructor() { }
+  constructor(private renderer : Renderer2) { }
 
   ngOnInit() {
-    
+  }
+
+  moveHoverElement(el : HTMLElement) {
+    let elRef = new ElementRef(el);
+
+    let nativeActionEl = this.elAction.nativeElement;
+    this.renderer.setStyle(nativeActionEl, 'left', el.offsetLeft.toString() + 'px');
+    this.renderer.setStyle(nativeActionEl, 'width', el.clientWidth.toString() + 'px');
   }
 
 }
